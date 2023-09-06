f6b17d07cb747bc97f89d8c2e48986a7ca484483
Michael <30936296+mtan93@users.noreply.github.com>
2023-09-06 16:56:07 +0100

    Update speedtestbasics.sh

--- speedtestbasics.sh
+++ speedtestbasics.sh
@@ -2,7 +2,9 @@ sudo apt-get install curl -y
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 sudo apt-get update
 sudo apt-get install speedtest iperf3 -y
+echo
 echo Speed Test Vodafone Bracknell":"
 speedtest -s 23389
+echo
 echo Speed Test Vodafone London":"
 speedtest -s 24281
